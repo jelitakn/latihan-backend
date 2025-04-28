cf052d8bc9cd83104bd0c93541ebc1ffee020827
jelitakn <jelitakhoirotunisa@gmail.com>
2025-04-28 10:52:50 +0700

    latihan mysql

---
+++ pertemuan 7/controller/mahasiswa.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const router = express.Router();
+const Mahasiswa = require('../models/mahasiswa');
+
+//GET
+router.get('/', async (req, res) => {
+    const mahasiswa = await Mahasiswa.findAll();
+    res.json(mahasiswa);
+});
+
+//GET by ID
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const mahasiswa = await Mahasiswa.findByPk(id);
+    if (mahasiswa) {
+        res.json(mahasiswa);
+    } else {
+        res.status(404).json({ message: 'Mahasiswa not found' });
+    }
+});
+
+//POST
+router.post('/', async (req, res) => {
+    const { nama, nim, jurusan} = req.body;
+    try {
+        const newMahasiswa = await Mahasiswa.create ({ nama, nim, jurusan });
+        res.status(201).json(newMahasiswa);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+//PUT 
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const { nama, nim, jurusan } = req.body;
+    try {
+       const update = await Mahasiswa.update(
+        { nama, nim, jurusan },
+        { where: { id } }
+       );
+       res.json({ message: 'Mahasiswa updated', update });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+//DELETE
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+       await Mahasiswa.destroy ({ where: { id } });
+       res.json({ message: 'Mahasiswa deleted' });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+module.exports = router;
---
+++ pertemuan 7/models/mahasiswa.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Mahasiswa = sequelize.define('Mahasiswa', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    nama: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    nim: {
+        type: DataTypes.STRING,
+        allowNull: false  
+    },
+    jurusan: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+module.exports = Mahasiswa;
